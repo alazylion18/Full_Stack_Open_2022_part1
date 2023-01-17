613de96356b24b7d12b4409f84e0ceedd96ba2e2
alazylion18 <LeonardoGiovanini@MacBook-Pro.lan>
2023-01-17 17:52:05 -0500

    Exercise 1.1

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-const Hello =(props) =>{
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+const Header = (props) =>{
+  return(
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
 }
 
-const App = () =>{
-  const name = "Peter"
-  const age = 10
+const Content = (props) =>{
+  return(
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>    
+    </>
+  )
+}
+
+const Total = (props) =>{
+  console.log(props)
+  return(
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
 
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10}/>
-      <Hello name="Daisy" age={age}/>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total total={exercises1+exercises2+exercises3}/>
     </>
   )
 }
