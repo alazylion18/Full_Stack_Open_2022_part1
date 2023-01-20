3419a76217bd38f2b73b3151153da2c533273dea
alazylion18 <LeonardoGiovanini@MacBook-Pro.lan>
2023-01-20 16:31:32 -0500

    Exercise 1.6

--- src/App.js
+++ src/App.js
@@ -1,67 +1,55 @@
-const Header = (props) =>{
-  //console.log(props.course.name)
-  return(
-    <>
-      <h1>{props.course.name}</h1>
-    </>
-  )
-}
+import { click } from '@testing-library/user-event/dist/click'
+import {useState} from 'react'
 
-const Content = (props) =>{
-  //console.log(props.parts.parts)
-  return(
-    <>
-      <Part part={props.parts.parts[0].name} exercise={props.parts.parts[0].exercises}/>
-      <Part part={props.parts.parts[1].name} exercise={props.parts.parts[1].exercises}/>
-      <Part part={props.parts.parts[2].name} exercise={props.parts.parts[2].exercises}/>
-    </>
-  )
-}
 
-const Part = (props) =>{
-  //console.log(props)
-  return(
-    <>
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) =>{
-  return(
-    <>
-      <p>Number of exercises {props.parts.parts[0].exercises+props.parts.parts[1].exercises+props.parts.parts[2].exercises}</p>
-    </>
-  )
+const Button = (props) => {
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
 }
 
 const App = () => {
-  const course = {
-  name: "Half Stack application development",
-  parts: [
-    {
-      name: "Fundamentals of React",
-      exercises:10
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
-    {
-      name: "State of a component",
-      exercises:14
-    }
-  ]
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
   }
+  
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+  
 
   return (
-    <>
-      <Header course={course}/>
-      <Content parts={course}/>
-      <Total parts={course}/>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>
+        good
+      </button>
+      <button onClick={handleNeutralClick}>
+        neutral
+      </button>
+      <button onClick={handleBadClick}>
+        bad
+      </button>
+      <br></br>
+      <h2>statistics</h2>
+      <p>
+        good {good} 
+      </p>
+      <p>
+        neutral {neutral}
+      </p>
+      <p>
+        bad {bad}
+      </p>
+     </div>
   )
 }
 
